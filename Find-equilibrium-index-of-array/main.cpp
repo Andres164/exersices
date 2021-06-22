9ef144f930fdd3cff9f9c863c822e68a5e1048a0
Andres Antonio Castro Beltran <andres435@gmail.com>
2021-06-21 19:44:38 -0600

    Finised the find equilibrim indexes of an array exersise with a time complexity of O(n), or linear timegit add . to accomplish this i needed to up the space complexity to linear space, also i had to see the logic, i could make it.

--- Find-equilibrium-index-of-array/main.cpp
+++ Find-equilibrium-index-of-array/main.cpp
@@ -5,31 +5,53 @@ using namespace std;
 
 int main()
 {
-    vector<int> myVector = {0, -3, 5, -4, -2, 3, 1, 0};
+    vector<int> myVector = { 0, -3, 5, -4, -2, 3, 1, 0 };
+    int myVectorLength = myVector.size();
 
     vector<int> equilibriumIndexes;
-    int sumOfElementsBeforeI = 0;
-    equilibriumIndexes.reserve(10);
-    for(int i = 0; i < myVector.size(); i++)
+    vector<int> sumsOfElementsBeforeIndex(myVectorLength, 0);
+    vector<int> sumsOfElementsAfterIndex(myVectorLength, 0);
+
+    for (int i = 1; i < myVectorLength; i++)
     {
+        vector<int>::iterator it_myVector = myVector.begin() + i;
+        vector<int>::iterator itInReverse_myVector = (myVector.end() - 1) - i;
+        vector<int>::iterator it_sumsOfElementsBeforeIndex = sumsOfElementsBeforeIndex.begin() + i;
+        vector<int>::iterator itInReverse_sumsOfElementsAfterIndex = (sumsOfElementsAfterIndex.end() -1) - i;
 
-        int sumOfElementsAfterI = 0;
-        if(i != (myVector.size() -1))
-        {
-            for(auto it_myVector = myVector.begin() +(i+1); it_myVector < myVector.end(); it_myVector++)
-                sumOfElementsAfterI += *it_myVector;
-        }
-
-        if(sumOfElementsBeforeI == sumOfElementsAfterI)
-            equilibriumIndexes.push_back(i);
-        vector<int>::iterator it_currentElement = myVector.begin() +i;
-        sumOfElementsBeforeI += *it_currentElement;
+        *it_sumsOfElementsBeforeIndex = (*(it_sumsOfElementsBeforeIndex - 1)) + (*(it_myVector - 1));
+        *itInReverse_sumsOfElementsAfterIndex = (*(itInReverse_myVector + 1)) + (*(itInReverse_sumsOfElementsAfterIndex + 1));
     }
-
-    for(int element : equilibriumIndexes)
-        cout << element << " ";
     cout << endl;
-    system("pause");
+    for (int i = 0; i < myVectorLength; i++)
+    {
+        vector<int>::iterator it_sumsOfElementsBeforeIndex = sumsOfElementsBeforeIndex.begin() + i;
+        vector<int>::iterator it_sumsOfElementsAfterIndex = sumsOfElementsAfterIndex.begin() + i;
+        if (*it_sumsOfElementsBeforeIndex == *it_sumsOfElementsAfterIndex)
+            equilibriumIndexes.push_back(i);
+        cout << endl << endl;
+        cout << *it_sumsOfElementsBeforeIndex << "  " << *it_sumsOfElementsAfterIndex << endl;
+    }
+    cout << endl;
+    /*for(int i = 0; i < myVector.size(); i++)
+        {
+
+            int sumOfElementsAfterI = 0;
+            if(i != (myVector.size() -1))
+            {
+                for(auto it_myVector = myVector.begin() +(i+1); it_myVector < myVector.end(); it_myVector++)
+                    sumOfElementsAfterI += *it_myVector;
+            }
+
+            if(sumOfElementsBeforeI == sumOfElementsAfterI)
+                equilibriumIndexes.push_back(i);
+            vector<int>::iterator it_currentElement = myVector.begin() +i;
+            sumOfElementsBeforeI += *it_currentElement;
+        }*/
+
+    for (int equilibriumIndex : equilibriumIndexes)
+        cout << equilibriumIndex << " ";
+    cout << endl;
 
     return 0;
 }
